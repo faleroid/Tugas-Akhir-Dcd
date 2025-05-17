e2d6b8fd42f035be3978bc575afe6b7708fadfa7
faleroid <naufalsatrioputra@gmail.com>
2025-05-17 15:07:12 +0700

    dikit lg final bos

---
+++ assests/scripts/galeri.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    new Glide('.glide', {
+        type: 'carousel'
+    }).mount()
+    console.log('glide')
+})
--- assests/scripts/script.js
+++ assests/scripts/script.js
@@ -1,30 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let dropdownToggle = document.getElementById("dropdown-toggle");
-    let dropdownMenu = document.getElementById("dropdown-menu");
-    let dropdownIcon = document.getElementById("dropdown-icon");
     let elements = document.querySelectorAll(".fade-in");
-
-    dropdownToggle.addEventListener("click", function (event) {
-        event.preventDefault();
-        dropdownMenu.classList.toggle("show");
-
-        dropdownIcon.textContent = dropdownMenu.classList.contains("show") ? " <" : " >";
-    });
-
-    document.addEventListener("click", function (event) {
-        if (!dropdownToggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
-            dropdownMenu.classList.remove("show");
-            dropdownIcon.textContent = " >"; 
-        }
-    });
-
-    dropdownMenu.querySelectorAll("a").forEach(item => {
-        item.addEventListener("click", function () {
-            dropdownMenu.classList.remove("show");
-            dropdownIcon.textContent = " >";
-        });
-    });
-
     let observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
